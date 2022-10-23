855d5fe6fa6347e01ca994a913b68123cae24af2
Gary O'Neall <gary@sourceauditor.com>
2022-10-23 11:18:05 -0700

    Add example with simpla Maven POM file
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

---
+++ example8/content/examplemaven/src/test/java/org/spdx/examplemaven/AppTest.java
@@ -0,0 +1,42 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2022 Source Auditor Inc.
+ */
+package org.spdx.examplemaven;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
