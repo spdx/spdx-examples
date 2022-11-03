8a61cd5a080e958af93d3cdfbbfdb6aa034003ff
Ivana Atanasova <iyovcheva@vmware.com>
2022-11-04 00:17:46 +0200

    Add Ruby example
    This change adds example11, containing Ruby Hello World bundle,
    a tar.gz distribution and two SPDX documents from source code and
    dist respectively
    
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>

---
+++ example12/content/hello/CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2022-11-03
+
+- Initial release
---
+++ example12/content/hello/lib/hello.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hello/version"
+
+module Hello
+  class Error < StandardError; end
+  puts "Hello World!"
+end
