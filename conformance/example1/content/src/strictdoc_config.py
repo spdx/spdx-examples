9ac34404b05095ead6ab6ad97fcb4827bc43a2d1
Gregory Shue <greg.shue@outlook.com>
2026-01-07 19:53:56 -0800

    Add shared sdoc grammar for system_verifications
    Signed-off-by: Gregory Shue <greg.shue@outlook.com>

--- conformance/example1/content/src/strictdoc_config.py
+++ conformance/example1/content/src/strictdoc_config.py
@@ -52,6 +52,7 @@ def create_config() -> ProjectConfig:
             "@sample_system_validations_grammar": "shared/sdoc/grammars/sample_system_validations.sgra",
             "@sample_interface_specifications_grammar": "shared/sdoc/grammars/sample_interface_specifications.sgra",
             "@sample_system_requirements_grammar": "shared/sdoc/grammars/sample_system_requirements.sgra",
+            "@sample_system_verifications_grammar": "shared/sdoc/grammars/sample_system_verifications.sgra",
         },
 
         # test_report_root_dict={
